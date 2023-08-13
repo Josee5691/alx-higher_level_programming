cd35a3185b12637c5a99039963c2d6f07144294b
Joseph <josephkariuki790@gmail.com>
2023-08-13 05:51:04 -0700

    maximum

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    biggest = my_list[0]
-    if(len(my_list) == 0):
+    if my_list is None or len(my_list) == 0:
         return None
+    max = my_list[0]
     for i in my_list:
-        if i > biggest:
-            biggest = i
-    return biggest
+        if i > max:
+            max = i
+    return max
