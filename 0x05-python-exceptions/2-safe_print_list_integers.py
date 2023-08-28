8cb9c7a660cc507ec7b33b57c6853a92b74bd612
Joseph Kariuki <josephkariuki790@gmail.com>
2023-08-28 20:57:14 +0300

    fix

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        count += 1
+        try:
+            print("{:d}".format(my_list[i]), end = "")
+        except(IndexError, ValueError, TypeError):
+            count -= 1
+        print()
+        return count
