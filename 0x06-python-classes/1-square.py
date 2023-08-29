342b04f27aaebb3eab95cedf2cb642b8809ee946
Joseph Kariuki <josephkariuki790@gmail.com>
2023-08-29 22:22:59 +0300

    fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
-class Square:
+"""This module defines the Square class."""
+
+class Squarei:
+    """
+    The Square class represents a square shape.
+
+    It contains a private instance attribute 'size'.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+
         self.__size = size
