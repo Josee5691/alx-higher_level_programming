b4d1a1809d3060513a8f2a3fbdd2fd13690aaa79
Joseph Kariuki <josephkariuki790@gmail.com>
2023-08-30 07:21:27 +0300

    fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""This module defines a Square class."""
+
 class Square:
+    """Square class with private size attribute and validation."""
+
     def __init__(self, size = 0):
+        """Initialize the Square instance with an optional size."""
+        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
