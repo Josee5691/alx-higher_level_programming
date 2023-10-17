bb37ad249f4a1105512621971b6fc681a5b13cee
Joseph Kariuki <josephkariuki790@gmail.com>
2023-10-17 13:56:18 +0300

    to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- Step 1: Convert the database to UTF8MB4
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Step 2: Convert the table to UTF8MB4
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Step 3: Convert the field to UTF8MB4
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
