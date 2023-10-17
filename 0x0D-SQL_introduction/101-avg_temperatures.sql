d6295ea918080ddd9e0a5cee8b0b580a0c73485f
Joseph Kariuki <josephkariuki790@gmail.com>
2023-10-17 17:21:37 +0300

    fix

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- average temparature
-SELECT city, AVG(value) AS avg_temp FROM temparatures GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
