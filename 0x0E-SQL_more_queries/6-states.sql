2d1fb87a0e6c3112cfc28f31fce337e9844de876
Joseph Kariuki <josephkariuki790@gmail.com>
2023-10-18 21:58:08 +0300

    fix

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,4 @@
 -- create database and table
---create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- activate database
 USE hbtn_0d_usa;
--- create table
-CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT UNIQUE NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
