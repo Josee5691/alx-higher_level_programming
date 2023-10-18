4b9138b205768e3d0e5d3080bfaf62939bd80670
Joseph Kariuki <josephkariuki790@gmail.com>
2023-10-18 22:45:46 +0300

    fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
--- creating a database and a user
+-- Script that creates the database hbtn_0d_2 and user user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
