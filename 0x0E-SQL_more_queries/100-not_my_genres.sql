30926f3e8ca63cf4cafebf479d06610b83c2d3cb
Joseph Kariuki <josephkariuki790@gmail.com>
2023-10-18 22:54:57 +0300

    fix

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,11 +1,3 @@
--- Script: 100-not_my_genres.sql
--- Description: Lists all genres not linked to the show Dexter.
--- Allowed editors: vi, vim, emacs
--- Environment: Ubuntu 20.04 LTS, MySQL 8.0.25
-
--- Use the hbtn_0d_tvshows database
-USE hbtn_0d_tvshows;
-
 -- Find the genres linked to the show Dexter
 SELECT genre_id
 FROM tv_show_genres
