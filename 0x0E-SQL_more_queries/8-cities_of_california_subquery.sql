c42dde2f7087fce2aa84ec8a262cf892df0c6ff2
Joseph Kariuki <josephkariuki790@gmail.com>
2023-10-18 22:32:18 +0300

    fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,3 @@
 -- creating sub queries
 USE hbtn_0d_usa;
--- main query
-SELECT * FROM cities WHERE state_id = 
-(
-	-- subquery
-	SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
+SELECT * FROM cities WHERE state_id = ( SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
