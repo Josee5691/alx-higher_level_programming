a67863b9dccee1d2da6da546e369417d88083333
Joseph Kariuki <josephkariuki790@gmail.com>
2023-12-14 19:24:06 +0300

    fix

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- show previleges
 -- previleges for user 1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- previledges for user 2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
