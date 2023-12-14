287dcaa2086d8e45c4cf50eb2990b1d5682a5f14
Joseph Kariuki <josephkariuki790@gmail.com>
2023-12-14 22:42:57 +0300

    fix

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,5 @@
--- Save this script in a file, e.g., rating_shows.sql
-
 -- Select all shows and their rating sum
-SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.id, tv_shows.title
