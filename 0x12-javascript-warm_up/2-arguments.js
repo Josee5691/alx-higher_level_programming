7936396e7c7848864bf3f1edb5d635d6a802dd1e
Joseph Kariuki <josephkariuki790@gmail.com>
2024-01-09 09:40:35 +0300

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,6 @@ const first = 'No argument';
 const second = 'Argument found';
 const third = 'Arguments found';
 const numOfArgs = process.argv.length;
-
 if (numOfArgs < 3) {
   console.log(first);
 }
