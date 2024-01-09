1f3943ff642f4bfa527343f074f8eddc05203020
Joseph Kariuki <josephkariuki790@gmail.com>
2024-01-09 09:15:43 +0300

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 const arr = process.argv;
 const length = arr.length;
-
 function secondBiggest (arr) {
   arr.sort((a, b) => (b - a));
   return arr[1];
 }
-
 if (length < 4) {
   console.log(0);
 }
