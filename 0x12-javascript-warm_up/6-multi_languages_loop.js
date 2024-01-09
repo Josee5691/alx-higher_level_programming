eb79003b5f2b598e6bff624b3ef9d62fc62651a0
Joseph Kariuki <josephkariuki790@gmail.com>
2024-01-09 10:12:07 +0300

    fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-
 const arr = ['C is fun', 'Python is cool', 'JavaScript is Amazing'];
-
-arr.forEach ((elem) => 
-  {
-	  console.log(elem);
-  }
-)
+arr.forEach ((elem) => {
+  console.log(elem);
+}
+);
