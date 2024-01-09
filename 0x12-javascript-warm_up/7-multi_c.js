57e3c83e7273ff83d8510ae414e500de776af142
Joseph Kariuki <josephkariuki790@gmail.com>
2024-01-09 10:50:54 +0300

    fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
-const x = parseInt(process.argv[2]);
-if (isNaN(x)) {
-  console.log('Missing number of occurences');
-}
-else {
-  for (let i = 0; i < x; i++) { console.log('C is fun'); }
+
+const num = process.argv[2];
+
+if (isNaN(parseInt(+num))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(+num); i++) {
+    console.log('C is fun');
+  }
 }
