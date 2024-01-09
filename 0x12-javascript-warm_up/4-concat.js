f6f2690d05864cd31f65a8bc08a0afe4a5464bfe
Joseph Kariuki <josephkariuki790@gmail.com>
2024-01-09 09:53:58 +0300

    fix

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const empty = 'undefined';
-console.log((process.argv[2] || empty) + " is " + (process.argv[3] || empty))
+console.log((process.argv[2] || empty) + " is " + (process.argv[3] || empty));
