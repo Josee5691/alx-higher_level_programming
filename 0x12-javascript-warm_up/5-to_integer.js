bfbddbbce3eb2feea14651521ce7b1bf4a407a06
Joseph Kariuki <josephkariuki790@gmail.com>
2024-01-09 10:05:21 +0300

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const statement = 'Not a number';
-const argument = parseInt(process.argv[2]);
+const argument = parseInt(+process.argv[2]);
 if (isNaN(argument)) {
   console.log('Not a number');
 } else {
