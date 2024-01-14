355ee9931a3da46ff2c8045255a32e1b5669ea93
Joseph Kariuki <josephkariuki790@gmail.com>
2024-01-14 16:57:18 +0300

    fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """
-connecting using mysql db
+here we are going to connect to a database and realize a
+query
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host = "localhost", user = sys.argv[1], port = 3306, passwd = sys.argv[2], db = sys.argv[3], charset = "utf8")
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         port=3306, passwd=sys.argv[2], db=sys.argv[3],
+                         charset="utf8")
     query = db.cursor()
-    query.execute("SELECT * FROM states WHERE BINARY NAME = '{}'\ORDER BY id ASC".format(x))
+    x = sys.argv[4]
+    query.execute("SELECT * FROM states WHERE BINARY NAME='{}'\
+                  ORDER BY id ASC".format(x))
     output = query.fetchall()
     for i in output:
         print(i)
