53300fd02bac8be6d03a7d13a3b9b699712e9fea
Joseph Kariuki <josephkariuki790@gmail.com>
2024-01-14 16:19:11 +0300

    fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-# script to list all states from database hbtn_0e_0_usa
-from sys import argv as av
-""" script to connect to db and access info """
+"""
+here we are going to connect to a database and realize a
+query
+"""
+import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
-    ac = len(av)
-    if ac < 4:
-        exit()
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=av[1],
-                           passwd=av[2],
-                           db=av[3],
-                           charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC;")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        if row[1][0] == "N":
-            print(row)
-    cur.close()
-    conn.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         port=3306, passwd=sys.argv[2], db=sys.argv[3],
+                         charset="utf8")
+    query = db.cursor()
+    query.execute("SELECT * FROM states ORDER BY id ASC")
+    output = query.fetchall()
+    for i in output:
+        if (i[1][0] == 'N'):
+            print(i)
+    query.close()
+    db.close()
